c5350017f18178e27305400637d98e73a125e49e
Manchot13 <osatoj.noshi@gmail.com>
2024-11-02 01:41:11 +0900

    mainの内容を保存

---
+++ src/app/index/page.tsx
@@ -0,0 +1,10 @@
+import { MyFirstARJS } from '@/features/ar/arjs/MyFirstARJS/MyFirstARJS';
+
+export default function MyARPage() {
+  return (
+    <div className='text-black'>
+      <h1>my first AR</h1>
+      <MyFirstARJS />
+    </div>
+  );
+}
